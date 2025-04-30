82ec75fb1fd745385b11fac2dec01fa46dd80567
inoue <pedro.inoue@somativa.com.br>
2025-04-30 15:51:09 -0300

    corrigindo

--- scripts/starter_apps.sh
+++ scripts/starter_apps.sh
@@ -18,9 +18,11 @@ apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release so
 echo "Installing Docker..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+UBUNTU_CODENAME=$(grep UBUNTU_CODENAME /etc/os-release | cut -d= -f2)
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable" \
   | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
